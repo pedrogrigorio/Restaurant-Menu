4bbd1ce7e60e64be9ea8a6b4f42a7015308f49d6
Pedro Grigorio <pedrooh.hxh@gmail.com>
2023-10-21 16:20:17 -0300

    implement get endpoint

--- restaurant-menu/src/main/java/com/pedrogrigorio/restaurantmenu/controller/FoodController.java
+++ restaurant-menu/src/main/java/com/pedrogrigorio/restaurantmenu/controller/FoodController.java
@@ -1,16 +1,27 @@
 package com.pedrogrigorio.restaurantmenu.controller;
 
+import com.pedrogrigorio.restaurantmenu.food.Food;
+import com.pedrogrigorio.restaurantmenu.food.FoodRepository;
+import com.pedrogrigorio.restaurantmenu.food.FoodResponseDTO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("food")
 public class FoodController {
 
+    @Autowired
+    private FoodRepository repository;
+
     @GetMapping
-    public String getAll() {
-        return "Hello, World";
+    public List<FoodResponseDTO> getAll() {
+        List<FoodResponseDTO> foodList = repository.findAll().stream().map(FoodResponseDTO::new).toList();
+        return foodList;
     }
 
 }
---
+++ restaurant-menu/src/main/java/com/pedrogrigorio/restaurantmenu/food/FoodResponseDTO.java
@@ -0,0 +1,8 @@
+package com.pedrogrigorio.restaurantmenu.food;
+
+public record FoodResponseDTO(long id, String title, String image, Integer price) {
+
+    public FoodResponseDTO(Food food) {
+        this(food.getId(), food.getTitle(), food.getImage(), food.getPrice());
+    }
+}
